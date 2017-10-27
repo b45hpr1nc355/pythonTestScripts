c81a5b562e13dc463bc872351bdf6b8d763cd6d7
b45hpr1nc355 <30328665+b45hpr1nc355@users.noreply.github.com>
2017-10-27 15:26:50 +0000

    Add files via upload

--- CowryCourierAPI.py
+++ CowryCourierAPI.py
@@ -9,6 +9,14 @@ import uuid
 
 import requests
 
+import urllib3
+
+urllib3.disable_warnings()
+
+http = urllib3.PoolManager(
+    cert_reqs='CERT_REQUIRED',
+    ca_certs='/path/to/your/certificate_bundle')
+
 
 PROTOCOL ="https"
 HOST ="api.misika247.com"
@@ -71,7 +79,7 @@ def listProviders():
 def checkWalletBalance():
     checkWalbal= {
         'api_key': '61b87502373649d09c01cffe9e1dcbd3',
-        'service_provider_id': '3006bee1f78047e8811f536122e2877a',
+        'api_secret': '3006bee1f78047e8811f536122e2877a',
         'source': 'api',
         'payment_provider_id': '4'
     }
@@ -80,33 +88,52 @@ def checkWalletBalance():
     return checkWalbal
 
 
-def customerAccountBalance():
+def checkAccountBalance():
     custAccBal= {
         'api_key': '61b87502373649d09c01cffe9e1dcbd3',
         'api_secret': '3006bee1f78047e8811f536122e2877a',
         'source': 'api',
-        'payment_provider_id': '4',
+        'provider_id': '4',
         'account_number':'0200313710'
     }
+    return custAccBal
+
+
+def accountLookup():
+    accLook= {
+        'api_key': '61b87502373649d09c01cffe9e1dcbd3',
+        'api_secret': '3006bee1f78047e8811f536122e2877a',
+        'source': 'api',
+        'provider_id': '4',
+        'account_number': '0200313710'
+    }
+    return accLook
+
+
 
 def depositFunds():
     depFunds= {
         'api_key': '61b87502373649d09c01cffe9e1dcbd3',
         'service_provider_id': '3006bee1f78047e8811f536122e2877a',
         'source': 'api',
-        'provider_id': '2',
-        'account_number': '233561077767',
-        'amnt': 1,
+        'provider_id':'4',
+        'payment_provider_id':'4',
+        'account_number': '233200313710',
+        'amount': 0.5,
         'currency': 'GHS',
-        'transaction_id': 'xxxxx',
+        'narration':'',
+        'transaction_id': '6578767',
         'notify_payer_after_processing': 1,
         'payer': 'Miriam Aidoo',
-        'payer_msisdn': '233561077767',
+        'payer_msisdn': '0504169784',
         'payer_email': 'miriam.aidoomensah@misika247.com',
+        'payer_address': '',
         'notify_recipient_on_success': 1,
         'recipient_name': 'Wendy Dua',
-        'recipient_msisdn': '233200313710',
-        'recipient_email': 'wendy.dua@misika247.com'
+        'recipient_msisdn': '0200313710',
+        'recipient_email': 'takyiwaa.dua@misika247.com',
+        'recipient_address':'',
+        'bank_refid':'6787687090'
     }
     #print "Amount sent: %s" %str(depFunds)
 
@@ -115,22 +142,25 @@ def depositFunds():
 
 def withdrawFunds():
     debitFunds= {
-        'api_key': '',
-        'service_provider_id': 'xxxx',
+        'api_key': '61b87502373649d09c01cffe9e1dcbd3',
+        'api_secret': '',
+        'service_provider_id': '3006bee1f78047e8811f536122e2877a',
         'source': 'api',
-        'provider_id': '0001',
-        'account_number': '233555555555',
-        'amount': '1',
+        'payment_provider_id': '4',
+        'account_number': '0200313710',
+        'amount': '0.5',
         'currency': 'GHS',
-        'transaction_id': 'xxxxx',
+        'narration':'',
+        'transaction_id': '17872',
         'notify_payer_after_processing': 1,
-        'payer': 'Wendy Dua',
-        'payer_msisdn': 'xxxxx',
-        'payer_email': 'xxxxx',
+        'debit_requester': 'GH4HUB',
+        'payer_msisdn': '0504169784',
+        'payer_email': 'miriam.aidoomensah@misika247.com',
+
         'notify_recipient_on_success': 1,
         'recipient_name': 'Baby Doe',
-        'recipient_msisdn': 'xxxx',
-        'recipient_email': 'name@somedomain.com'
+        'recipient_msisdn': '0200313710',
+        'recipient_email': 'takyiwaa.dua@misika247.com',
     }
     #print "Amount received: %s" %str(debitFunds)
 
@@ -162,8 +192,11 @@ if __name__=='__main__':
     checkWalbal=checkWalletBalance()
     call_api_interface(checkWalbal, "cowrycourier", "checkWalletBalance")
 
-    custAccBal=customerAccountBalance()
-    call_api_interface(custAccBal, "cowrycourier", "customerAccountBalance")
+    custAccBal=checkAccountBalance()
+    call_api_interface(custAccBal, "cowrycourier", "checkAccountBalance")
+
+    accLook=accountLookup()
+    call_api_interface(accLook, "cowrycourier", "accountLookup")
 
     #depFunds=depositFunds()
     #call_api_interface(depFunds, "cowrycourier", "depositFunds")
