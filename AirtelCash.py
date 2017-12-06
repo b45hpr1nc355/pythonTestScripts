5e6663cd23ddc35636721a3770b780e25d33523b
b45hpr1nc355 <30328665+b45hpr1nc355@users.noreply.github.com>
2017-12-06 15:20:20 +0000

    Add files via upload

--- AirtelCash.py
+++ AirtelCash.py
@@ -2,19 +2,27 @@
 
 import requests
 
-payload={"msisdn":"233268951271",
-         "amount":"1",
+payload={"msisdn":"233561077767",
+         "amount":"0.2",
          "mno":"Airtel",
-         "token":"",
-         "narration":"Credit",
-         "misika247_transaction_id":"00009",
-         "trxid":"00008"
-        }
+         "token":""
+         }         
 
-#url = "http://cbs2p0:24766/cbs/nsano/creditWallet/"
-url = "http://cbs2p0:24766/cbs/nsano/debitWallet/"
+url = "http://cbs2p0:24766/cbs/nsano_test/creditWallet/"
+#url = "http://cbs2p0:24766/cbs/nsano/debitWallet/"
 call_cbs = requests.post(url,json=payload,verify=False,timeout=60)
 print(call_cbs.text)
 
 #meSika --> 233268951271
 #CEO --> 0561077767
+
+
+
+
+#"msisdn":"233268951271",
+        # "amount":"1",
+        # "mno":"Airtel",
+        # "token":"",
+        # "narration":"Credit",
+        # "misika247_transaction_id":"00019",
+        # "trxid":"00007"
