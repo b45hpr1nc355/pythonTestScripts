11dc48480fbbc3cecd6b1985d9f26f20350fbde2
b45hpr1nc355 <30328665+b45hpr1nc355@users.noreply.github.com>
2018-01-12 21:14:05 +0000

    Add files via upload

--- CowryGongSMSAPI.py
+++ CowryGongSMSAPI.py
@@ -25,7 +25,8 @@ def create_uuid():
 
 
 def call_api_interface(api_postSMS, module, action):
-    print("SMS delivered: \n")
+    print(" ")
+    print("Your payload: \n")
     print(api_postSMS)
     print(" ")
 
@@ -43,7 +44,7 @@ def call_api_interface(api_postSMS, module, action):
     resp_dict = req.json()
 
     print("Respond from API call = %s " % resp_dict)
-
+    print(" ")
 
 #def generateAPIkey():
  #   getLoginDets = {
@@ -61,50 +62,53 @@ def call_api_interface(api_postSMS, module, action):
 def getBalance():
     checkSMSbal = {
         "api_key": "bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", # "61b87502373649d09c01cffe9e1dcbd3",
-        "service_provider_id": "TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg", # "3006bee1f78047e8811f536122e2877a",
+        "api_secret": "TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg", # "3006bee1f78047e8811f536122e2877a",    #service_provider_id
+        "client_id":"000001",
         "source": "api"
-    }
-    print("Your SMS balance: %s" % str(checkSMSbal))
-
+        }
     return checkSMSbal
+
+
 
 
 def getRoutes():
     getSMSroutes = {
         "api_key": "bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", # "61b87502373649d09c01cffe9e1dcbd3",
-        "service_provider_id": "TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg", # "3006bee1f78047e8811f536122e2877a",
+        "api_secret": "TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg", # "3006bee1f78047e8811f536122e2877a",  #service_provider_id
+        "client_id":"000001",
         "source": "api"
-    }
-    print("The configured SMS routes: %s" % str(getSMSroutes))
+      }
     return getSMSroutes
+
+
 
 
 def postBulkMessage():
     postbulkSMS = {
         "api_key": "bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", #"61b87502373649d09c01cffe9e1dcbd3",
-        "service_provider_id": "TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg", #"3006bee1f78047e8811f536122e2877a",
+        "api_secret": "TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg", #"3006bee1f78047e8811f536122e2877a",    #service_provider_id
+        "client_id":"000001",
         "source": "api",
+        "callback": 1,
         "routeID": "297b53667470e24ca41a18c8105f6260", # "8f1df72288915292efcf456df43e5b20",
         "messages": [{"messageID": "4c2c60d1", "text": "Tests from Petra", "phonenumber": "233504169784"},
-                     {"messageID": "d87801fe", "text": "Tests from Petra", "phonenumber": "233200313710"},
+                     {"messageID": "d87801fe", "text": "Tests from Petra", "phonenumber": "233208027401"},
                      {"messageID": "d87801fe", "text": "Tests from Petra", "phonenumber": "233555055956"}],
         "senderID": "Petra",
         "messageType": "SMS",
-        "callback": 1,
-        "dlr-url": " ",
+        #"dlr-url": " ",
         "priority-level": "HIGH"
-    }
-    print("Your bulk message creds: %s" % str(postbulkSMS))
-
+        }
     return postbulkSMS
+
+
 
 
 def postMessage():
     postMess = {
         "api_key": "bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", #"61b87502373649d09c01cffe9e1dcbd3",
-        #"api_secret":"TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg",
-        "service_provider_id": "TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg",  #"3006bee1f78047e8811f536122e2877a",
-        "client_id":"",
+        "api_secret":"TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg",   #"3006bee1f78047e8811f536122e2877a",   #service_provider_id
+        "client_id":"000001",
         "source": "api",
         "callback": 1,
         "routeID": "297b53667470e24ca41a18c8105f6260", # "8f1df72288915292efcf456df43e5b20",
@@ -114,44 +118,98 @@ def postMessage():
         "messageType": "SMS",
         "phonenumber": "233200313710",
         "priority-level": "HIGH"
-    }
-    print("Your SMS creds: %s" % str(postMess))
-
+        }
     return postMess
+
+
+
+
+def getOutboxMessageSize():
+    getOMSize = {
+        "api_key": "bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", #"61b87502373649d09c01cffe9e1dcbd3",
+        "api_secret":"TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg",   #"3006bee1f78047e8811f536122e2877a",   #service_provider_id
+        "client_id":"000001",
+        "source": "api",
+        "start_date" :"",
+        "end_date":""
+    }
+    return getOMSize
+
+
+
+
+def getOutboxMessages():
+    getOMess = {
+        "api_key": "bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", #"61b87502373649d09c01cffe9e1dcbd3",
+        "api_secret":"TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg",   #"3006bee1f78047e8811f536122e2877a",   #service_provider_id
+        "client_id":"000001",
+        "source": "api",
+        "start_date" :"",
+        "end_date":"",
+        "data_size": 100,
+        "page":1
+    }
+    return getOMess
+
+
 
 
 def getMessageStatus():
     getStatus = {
         "api_key": "bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", # "61b87502373649d09c01cffe9e1dcbd3",
-        "service_provider_id": "TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg",  # "3006bee1f78047e8811f536122e2877a",
+        "api_secret": "TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg",  # "3006bee1f78047e8811f536122e2877a",     #service_provider_id
+        "client_id":"000001",
         "source": "api",
         "messageID": "4c2c60d1"
     }
-    print("Your message status is: %s" % str(getStatus))
-
     return getStatus
 
 
 
+
+def updateDlrEndpoint():
+    updateDlr  = {
+        "api_key": "bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", # "61b87502373649d09c01cffe9e1dcbd3",
+        "api_secret": "TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg",  # "3006bee1f78047e8811f536122e2877a",     #service_provider_id
+        "client_id":"000001",
+        "source": "api",
+        "endpoint": "https://misika247.com"
+    }
+    return updateDlr
+
+
+
+
 if __name__ == "__main__":
+    
+    print("")
     print ("UUID: " + create_uuid())
 
     # getLoginDets= generateAPIkey()
     # call_api_interface(getLoginDets, "cowrygong", "sms")
 
-    #checkSMSBal = getBalance()
-    #call_api_interface(checkSMSBal, "cowrygong", "getBalance")
+    checkSMSBal = getBalance()
+    call_api_interface(checkSMSBal, "cowrygong", "getBalance")
 
-    #getSMSr=getRoutes()
-    #call_api_interface(getSMSr, "cowrygong", "getRoutes")
+    getSMSr=getRoutes()
+    call_api_interface(getSMSr, "cowrygong", "getRoutes")
 
-    postSMS=postMessage()
-    call_api_interface(postSMS, "cowrygong", "postMessage")
+    #postSMS=postMessage()
+    #call_api_interface(postSMS, "cowrygong", "postMessage")
 
-    postBSMS=postBulkMessage()
-    call_api_interface(postBSMS, "cowrygong", "postBulkMessage")
+    #postBSMS=postBulkMessage()
+    #call_api_interface(postBSMS, "cowrygong", "postBulkMessage")
 
-    # getStat=getMessageStatus()
-    # call_api_interface(getStat, "cowrygong", "getMessageStatus")
+    #getOMSize=getOutboxMessageSize()
+    #call_api_interface(getOMSize, "cowrygong", "getOutboxMessageSize")
+
+    #getOMess=getOutboxMessages()
+    #call_api_interface(getOMess, "cowrygong", "getOutboxMessages")
+
+    #updateDlr=updateDlrEndpoint()
+    #call_api_interface(updateDlr, "cowrygong", "updateDlrEndpoint")
+
+    #getStat=getMessageStatus()
+    #call_api_interface(getStat, "cowrygong", "getMessageStatus")
 
 
