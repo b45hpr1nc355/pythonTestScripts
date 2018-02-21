bda41a5bd47ad141affb7a509176fb60835ded4b
b45hpr1nc355 <30328665+b45hpr1nc355@users.noreply.github.com>
2018-02-21 18:21:16 +0000

    Add files via upload

--- CowryGongSMSAPI.py
+++ CowryGongSMSAPI.py
@@ -14,7 +14,7 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 PROTOCOL = "https"
-HOST = "api.misika247.com", #"sandbox.misika247.com" # "api.misika247.com"
+HOST = "41.207.254.75" #"api.misika247.com", #"sandbox.misika247.com" # "api.misika247.com"
 PORT = "24799"
 
 
@@ -61,9 +61,9 @@ def call_api_interface(api_postSMS, module, action):
 
 def getBalance():
     checkSMSbal = {
-        "api_key": "WTy9TlKv3mo0QuhOC9XHK5RgXTCvRMQAwhFSkQDUr6jdfQI6osjfZRbHo", #"bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", #"61b87502373649d09c01cffe9e1dcbd3",
-        "service_provider_id": "dl7z5Oxgwn6to3JAo1gVY3jNjjG3dnIHZaQlLxios", #"TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg", #"3006bee1f78047e8811f536122e2877a"
-        "client_id":"000001",
+        "api_key": "WTy9TlKv3mo0QuhOC9XHK5RgXTCvRMQAwhFSkQDUr6jdfQI6osjfZRbHo", #"61b87502373649d09c01cffe9e1dcbd3"
+        "service_provider_id": "dl7z5Oxgwn6to3JAo1gVY3jNjjG3dnIHZaQlLxios",  #"3006bee1f78047e8811f536122e2877a"
+        #"client_id":"000001",
         "source": "api"
         }
     return checkSMSbal
@@ -74,7 +74,7 @@ def getBalance():
 def getRoutes():
     getSMSroutes = {
         "api_key": "WTy9TlKv3mo0QuhOC9XHK5RgXTCvRMQAwhFSkQDUr6jdfQI6osjfZRbHo",  #"bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", #"61b87502373649d09c01cffe9e1dcbd3",
-        "service_provider_id": "dl7z5Oxgwn6to3JAo1gVY3jNjjG3dnIHZaQlLxios", #"TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg", #"3006bee1f78047e8811f536122e2877a",
+        "service_provider_id": "dl7z5Oxgwn6to3JAo1gVY3jNjjG3dnIHZaQlLxios",  #"3006bee1f78047e8811f536122e2877a", #"TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg", #"3006bee1f78047e8811f536122e2877a",
         #"client_id":"000001",
         "source": "api"
       }
@@ -85,16 +85,16 @@ def getRoutes():
 
 def postBulkMessage():
     postbulkSMS = {
-        "api_key": "bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", #"61b87502373649d09c01cffe9e1dcbd3",
-        "api_secret": "TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg", #"3006bee1f78047e8811f536122e2877a",    #service_provider_id
-        "client_id":"000001",
+        "api_key": "WTy9TlKv3mo0QuhOC9XHK5RgXTCvRMQAwhFSkQDUr6jdfQI6osjfZRbHo",  #"bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", #"61b87502373649d09c01cffe9e1dcbd3",
+        "service_provider_id": "dl7z5Oxgwn6to3JAo1gVY3jNjjG3dnIHZaQlLxios",  #"TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg", #"3006bee1f78047e8811f536122e2877a",    #service_provider_id    "api_secret"
+        #"client_id":"000001",
         "source": "api",
         "callback": 1,
-        "routeID": "297b53667470e24ca41a18c8105f6260", # "8f1df72288915292efcf456df43e5b20",
-        "messages": [{"messageID": "4c2c60d1", "text": "Tests from Petra", "phonenumber": "233504169784"},
-                     {"messageID": "d87801fe", "text": "Tests from Petra", "phonenumber": "233208027401"},
-                     {"messageID": "d87801fe", "text": "Tests from Petra", "phonenumber": "233555055956"}],
-        "senderID": "Petra",
+        "routeID": "b92c230b8ddac49e9f4e8cfa1de2c0a2",  #"297b53667470e24ca41a18c8105f6260", # "8f1df72288915292efcf456df43e5b20",
+        "messages": [{"messageID": "4c2c60d1", "text": "Tests from Kimpton", "phonenumber": "233504169784"},
+                     {"messageID": "d87801fe", "text": "Tests from Kimpton", "phonenumber": "233200313710"},
+                     {"messageID": "d87801fe", "text": "Tests from Kimpton", "phonenumber": "233555055956"}],
+        "senderID": "Kimpton",
         "messageType": "SMS",
         #"dlr-url": " ",
         "priority-level": "HIGH"
@@ -106,14 +106,14 @@ def postBulkMessage():
 
 def postMessage():
     postMess = {
-        "api_key": "bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", #"61b87502373649d09c01cffe9e1dcbd3",
-        "api_secret":"TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg",   #"3006bee1f78047e8811f536122e2877a",   #service_provider_id
-        "client_id":"000001",
+        "api_key": "WTy9TlKv3mo0QuhOC9XHK5RgXTCvRMQAwhFSkQDUr6jdfQI6osjfZRbHo",  #"bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", #"61b87502373649d09c01cffe9e1dcbd3",
+        "service_provider_id": "dl7z5Oxgwn6to3JAo1gVY3jNjjG3dnIHZaQlLxios",   #"TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg",   #"3006bee1f78047e8811f536122e2877a",   #service_provider_id
+        #"client_id":"000001",
         "source": "api",
         "callback": 1,
-        "routeID": "297b53667470e24ca41a18c8105f6260", # "8f1df72288915292efcf456df43e5b20",
-        "text": "Petra Tests",
-        "senderID": "Petra",
+        "routeID":"b92c230b8ddac49e9f4e8cfa1de2c0a2",     #"297b53667470e24ca41a18c8105f6260", # "8f1df72288915292efcf456df43e5b20",
+        "text": "Kimpton Tests",
+        "senderID": "Kimpton",
         "messageID": "4c2c60d1",
         "messageType": "SMS",
         "phonenumber": "233200313710",
@@ -156,9 +156,9 @@ def getOutboxMessages():
 
 def getMessageStatus():
     getStatus = {
-        "api_key": "bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", # "61b87502373649d09c01cffe9e1dcbd3",
-        "api_secret": "TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg",  # "3006bee1f78047e8811f536122e2877a",     #service_provider_id
-        "client_id":"000001",
+        "api_key": "WTy9TlKv3mo0QuhOC9XHK5RgXTCvRMQAwhFSkQDUr6jdfQI6osjfZRbHo",  #"bNm0YN3P0lTwTsNI6bHnkLbghK09jjrw7bmuGERG1fiqMgXkqPRdITAnsAI", # "61b87502373649d09c01cffe9e1dcbd3",
+        "service_provider_id": "dl7z5Oxgwn6to3JAo1gVY3jNjjG3dnIHZaQlLxios",      #"TYrDfyu1P7YPDXtE6uMFT3LRPKZU2kL7EmmsWI5jqg",  # "3006bee1f78047e8811f536122e2877a",     #service_provider_id   "api_secret"
+        #"client_id":"000001",
         "source": "api",
         "messageID": "4c2c60d1"
     }
@@ -191,8 +191,8 @@ if __name__ == "__main__":
     checkSMSBal = getBalance()
     call_api_interface(checkSMSBal, "cowrygong", "getBalance")
 
-    getSMSr=getRoutes()
-    call_api_interface(getSMSr, "cowrygong", "getRoutes")
+    #getSMSr=getRoutes()
+    #call_api_interface(getSMSr, "cowrygong", "getRoutes")
 
     #postSMS=postMessage()
     #call_api_interface(postSMS, "cowrygong", "postMessage")
@@ -209,7 +209,7 @@ if __name__ == "__main__":
     #updateDlr=updateDlrEndpoint()
     #call_api_interface(updateDlr, "cowrygong", "updateDlrEndpoint")
 
-    #getStat=getMessageStatus()
-    #call_api_interface(getStat, "cowrygong", "getMessageStatus")
+    getStat=getMessageStatus()
+    call_api_interface(getStat, "cowrygong", "getMessageStatus")
 
 
--- MTNCash.py
+++ MTNCash.py
@@ -1,12 +1,12 @@
 #!/bin/env python
 import requests
 
-payload={"msisdn":"233548434029",
-         "amount":"0.2",
-         "misika247_transaction_id":"313228",
-	 "narration":"Credit" }
+payload={"msisdn":"233548434029"}
+         #"amount":"0.2",
+         #"misika247_transaction_id":"313228",
+	 #"narration":"Credit" }
 
-url = "http://cbs2p0:24766/cbs/mtncash/creditWallet/"
+url = "http://cbs2p0:24766/cbs/mtncash/validateAccountHolder/"                        #creditWallet/
 #url = "http://cbs2p0:24766/cbs/mtncash/debitWallet/"
 #url = "http://cbs.misika247.com:24766/cbs/mtncash/debitWallet/"
 call_cbs = requests.post(url,json=payload,verify=False,timeout=60)
@@ -15,3 +15,10 @@ print(call_cbs.text)
 #Hester ---> 0545396737
 #meSika ---> 0557826718
 #Edna ---> 233548434029
+
+
+#{"msisdn":"233548434029",
+  #       "amount":"0.2",
+ #        "misika247_transaction_id":"313228",
+#         "narration":"Credit" }
+
